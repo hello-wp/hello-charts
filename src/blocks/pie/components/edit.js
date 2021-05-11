8d69f88736993e6118025b9a65b64cc634e00d8a
Luke Carbis <luke@carb.is>
2021-05-12 08:35:29 +1000

    Add pie chart components

---
+++ src/blocks/pie/components/edit.js
@@ -0,0 +1,113 @@
+/**
+ * BLOCK: Pie Chart
+ */
+
+/**
+ * Components and dependencies.
+ */
+import { Pie } from 'react-chartjs-2';
+import { ChartStyles, DataStyles } from '.';
+import { EditDataButton, EditDataModal, EditDataToolbar, Legend } from '../../../common/components';
+import { randomColor, randomValues } from '../../../common/helpers';
+
+/**
+ * WordPress dependencies.
+ */
+const { __ } = wp.i18n;
+const { Component } = wp.element;
+const { BlockControls, InspectorControls, RichText } = wp.blockEditor;
+
+export default class Edit extends Component {
+	constructor( props ) {
+		super( props );
+
+		// Setup the attributes
+		const {
+			attributes: { chartData },
+			clientId,
+			setAttributes,
+		} = this.props;
+
+		const parsedData = JSON.parse( chartData );
+
+		this.state = { editorOpen: false };
+
+		parsedData.datasets.forEach( ( dataset ) => {
+			if ( 'generate' === dataset.data[ 0 ] ) {
+				dataset.data = randomValues( 3, 1, 10 );
+			}
+
+			if ( ! dataset.hasOwnProperty( 'backgroundColor' ) ) {
+				dataset.borderColor = [];
+				dataset.backgroundColor = [];
+				dataset.data.forEach( () => {
+					const color = randomColor();
+					dataset.borderColor.push( color );
+					dataset.backgroundColor.push( color );
+				} );
+			}
+		} );
+
+		setAttributes( {
+			chartType: 'pie',
+			blockId: clientId,
+			chartData: JSON.stringify( parsedData ),
+		} );
+	}
+
+	toggleEditor() {
+		this.setState( { editorOpen: this.state.editorOpen ? false : true } );
+	}
+
+	render() {
+		const {
+			attributes: {
+				title,
+				blockId,
+				chartData,
+				chartOptions,
+			},
+			className,
+			setAttributes,
+		} = this.props;
+
+		const parsedData = JSON.parse( chartData );
+		const parsedOptions = JSON.parse( chartOptions );
+
+		this.toggleEditor = this.toggleEditor.bind( this );
+
+		return (
+			<>
+				<InspectorControls key="inspector">
+					<EditDataButton toggleEditor={ this.toggleEditor } />
+					<ChartStyles { ...this.props } />
+					<DataStyles { ...this.props } />
+					<Legend { ...this.props } />
+				</InspectorControls>
+				<BlockControls>
+					<EditDataToolbar toggleEditor={ this.toggleEditor } />
+				</BlockControls>
+				<div className={ className } key="preview">
+					<div className="wrapper">
+						<RichText
+							tagName="h3"
+							placeholder={ __( 'Pie Chart' ) }
+							value={ title }
+							allowedFormats={ [] }
+							withoutInteractiveFormatting={ true }
+							onChange={ ( value ) => setAttributes( { title: value } ) }
+						/>
+						{ ! this.state.editorOpen && (
+							<div className="chart">
+								<Pie id={ blockId } data={ parsedData } options={ parsedOptions } />
+							</div>
+						) }
+						{ this.state.editorOpen && (
+							<EditDataModal toggleEditor={ this.toggleEditor } { ...this.props } />
+						) }
+					</div>
+				</div>
+			</>
+		);
+	}
+}
