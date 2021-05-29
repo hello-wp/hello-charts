bc592add8c1f2f0a1bdde30de4a4c7ac84fec7ca
Luke Carbis <luke@carb.is>
2021-05-30 06:39:04 +1000

    Some small edit data modal fixes

--- src/blocks/radar/index.js
+++ src/blocks/radar/index.js
@@ -65,7 +65,7 @@ registerBlockType( 'hello-charts/block-radar', {
 				labels: [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ],
 				datasets: [
 					{
-						label: __( 'Dataset', 'hello-charts' ),
+						label: __( 'Data Set', 'hello-charts' ),
 						fill: true,
 						borderWidth: 3,
 						pointRadius: 3,
