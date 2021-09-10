c509ed3122cd5861b85c9897d4f6949a02d6e169
Luke Carbis <luke@carb.is>
2021-09-10 15:45:51 +1000

    Divide data contextual menu into groups

--- js/common/components/edit-data-contextual-menu.js
+++ js/common/components/edit-data-contextual-menu.js
@@ -55,48 +55,54 @@ export default class EditDataContextualMenu extends Component {
 				popoverProps={ { noArrow: false } }
 			>
 				{ ( { onClose } ) => (
-					<MenuGroup>
-						<MenuItem
-							icon={ `table-${ itemIconKey }-after` }
-							onClick={ () => duplicate( index ) }
-							onBlur={ ( event ) => maybeClose( event, onClose ) }
-						>
-							{
-								/* Translators: Placeholder describes a row ("row") or column ("data set") */
-								sprintf( __( 'Duplicate %s', 'hello-charts' ), itemName )
-							}
-						</MenuItem>
-						<MenuItem
-							icon={ `table-${ itemIconKey }-before` }
-							onClick={ () => add( index ) }
-							onBlur={ ( event ) => maybeClose( event, onClose ) }
-						>
-							{
-								/* Translators: Placeholder describes a row ("row") or column ("data set") */
-								sprintf( __( 'Add %s Before', 'hello-charts' ), itemName )
-							}
-						</MenuItem>
-						<MenuItem
-							icon={ `table-${ itemIconKey }-after` }
-							onClick={ () => add( index + 1 ) }
-							onBlur={ ( event ) => maybeClose( event, onClose ) }
-						>
-							{
-								/* Translators: Placeholder describes a row ("row") or column ("data set") */
-								sprintf( __( 'Add %s After', 'hello-charts' ), itemName )
-							}
-						</MenuItem>
-						<MenuItem
-							icon={ `table-${ itemIconKey }-delete` }
-							onClick={ () => remove( index ) }
-							onBlur={ ( event ) => maybeClose( event, onClose ) }
-						>
-							{
-								/* Translators: Placeholder describes a row ("row") or column ("data set") */
-								sprintf( __( 'Delete %s', 'hello-charts' ), itemName )
-							}
-						</MenuItem>
-					</MenuGroup>
+					<>
+						<MenuGroup>
+							<MenuItem
+								icon={ `table-${ itemIconKey }-before` }
+								onClick={ () => add( index ) }
+								onBlur={ ( event ) => maybeClose( event, onClose ) }
+							>
+								{
+									/* Translators: Placeholder describes a row ("row") or column ("data set") */
+									sprintf( __( 'Add %s Before', 'hello-charts' ), itemName )
+								}
+							</MenuItem>
+							<MenuItem
+								icon={ `table-${ itemIconKey }-after` }
+								onClick={ () => add( index + 1 ) }
+								onBlur={ ( event ) => maybeClose( event, onClose ) }
+							>
+								{
+									/* Translators: Placeholder describes a row ("row") or column ("data set") */
+									sprintf( __( 'Add %s After', 'hello-charts' ), itemName )
+								}
+							</MenuItem>
+						</MenuGroup>
+						<MenuGroup>
+							<MenuItem
+								icon="controls-repeat"
+								onClick={ () => duplicate( index ) }
+								onBlur={ ( event ) => maybeClose( event, onClose ) }
+							>
+								{
+									/* Translators: Placeholder describes a row ("row") or column ("data set") */
+									sprintf( __( 'Duplicate %s', 'hello-charts' ), itemName )
+								}
+							</MenuItem>
+						</MenuGroup>
+						<MenuGroup>
+							<MenuItem
+								icon={ `table-${ itemIconKey }-delete` }
+								onClick={ () => remove( index ) }
+								onBlur={ ( event ) => maybeClose( event, onClose ) }
+							>
+								{
+									/* Translators: Placeholder describes a row ("row") or column ("data set") */
+									sprintf( __( 'Delete %s', 'hello-charts' ), itemName )
+								}
+							</MenuItem>
+						</MenuGroup>
+					</>
 				) }
 			</DropdownMenu>
 		);
